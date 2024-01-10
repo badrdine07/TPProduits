2e4a145838af6987f271f3cb924c8013df2f0acd
badrdine07 <fadibadr969@gmail.com>
2024-01-10 15:11:09 +0100

    creation de la classe Produit

---
+++ GestionProd/src/main/java/com/prod/Produit.java
@@ -0,0 +1,21 @@
+package com.prod;
+
+public class Produit {
+    private Long id;
+    private String nom;
+    private double prix;
+    private int quantite;
+
+    // Constructeur
+    public Produit(Long id, String nom, double prix, int quantite) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.quantite = quantite;
+    }
+
+
+
+   
+}
+
